e4858dff5d1734779a81afd37148470b76fa284b
geethamGT3RS <geetham.gsc@gmail.com>
2024-04-04 13:50:18 +0530

    working connection

--- osc.sh
+++ osc.sh
@@ -1,5 +1,3 @@
-#echo this in red color "Open source software"
-# echo 2 rows of - as border
 echo "------------------------------------------------------------------------------"
 echo -e "\e[31mOpen source software\e[0m"
 echo "------------------------------------------------------------------------------"
@@ -7,6 +5,8 @@ echo "This is a script to run the oscilloscope"
 echo "------------------------------------------------------------------------------"
 echo -e "\e[31mDeveloped by: Geetham Talluri"
 echo -e "Git Repository: https://github.com/geethamGT3RS/2Mhz_PC_Oscilloscope.git\e[0m"
+echo "------------------------------------------------------------------------------"
+echo "dependencies: python3, wiringPi"
 echo "------------------------------------------------------------------------------"
 
 
